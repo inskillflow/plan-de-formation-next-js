8f01a80c02c0e9d3371e4105d329662de1cbaf2f
inskillflow <formationselasticaws@gmail.com>
2025-10-15 20:02:54 -0400

    Update and rename 02.md to 02-Liens-des-projets-GitHub-Stack-React-Next.js

---
+++ 02-Liens-des-projets-GitHub-Stack-React-Next.js
@@ -0,0 +1,42 @@
+## Projets (Next.js)
+
+* [https://github.com/hrhouma1/next-01-projet01-in-memory](https://github.com/hrhouma1/next-01-projet01-in-memory) (sw2)
+* [https://github.com/hrhouma1/next-01-projet02-partie1](https://github.com/hrhouma1/next-01-projet02-partie1) (sw3)
+* [https://github.com/hrhouma1/next-01-projet02-partie2](https://github.com/hrhouma1/next-01-projet02-partie2) (sw3)
+* [https://github.com/hrhouma1/next-01-projet02-partie3](https://github.com/hrhouma1/next-01-projet02-partie3) (sw3)
+* [https://github.com/hrhouma1/next-01-projet03-in-prisma](https://github.com/hrhouma1/next-01-projet03-in-prisma) (sw2)
+* [https://github.com/hrhouma1/next-01-projet04-clerk-app-demo1](https://github.com/hrhouma1/next-01-projet04-clerk-app-demo1)
+
+## Déploiement / Pipelines
+
+* [https://github.com/hrhouma1/next-02-projet01-next-auth-azure-ghaction](https://github.com/hrhouma1/next-02-projet01-next-auth-azure-ghaction) (avancé) — Azure + GitHub Actions + domaine (NextAuth)
+* [https://github.com/hrhouma1/next-02-projet02-next-jwt-hostinger-ghaction](https://github.com/hrhouma1/next-02-projet02-next-jwt-hostinger-ghaction) — Hostinger + GitHub Actions + domaine (**JWT**)
+
+## Évaluations / Labs
+
+* [https://github.com/hrhouma1/next-01-evaluation-lab2](https://github.com/hrhouma1/next-01-evaluation-lab2)
+* [https://github.com/hrhouma1/next-01-evaluation-du-lab2](https://github.com/hrhouma1/next-01-evaluation-du-lab2) (**JWT**)
+* [https://github.com/hrhouma1/next-01-evaluation-du-lab2-projet-final](https://github.com/hrhouma1/next-01-evaluation-du-lab2-projet-final) (**JWT**)
+* [https://github.com/hrhouma1/sw-1/blob/main/Evaluations/04-Laboratoire2.md](https://github.com/hrhouma1/sw-1/blob/main/Evaluations/04-Laboratoire2.md)
+
+## Auth / Clerk / Docs
+
+* [https://github.com/hrhouma1/next-01-evaluation-du-lab2-auth-clerk-1](https://github.com/hrhouma1/next-01-evaluation-du-lab2-auth-clerk-1)
+
+  * Docs “THÉORIE” :
+
+    * [https://github.com/hrhouma1/next-01-evaluation-du-lab2-auth-clerk-1/tree/main/01-documentation-01](https://github.com/hrhouma1/next-01-evaluation-du-lab2-auth-clerk-1/tree/main/01-documentation-01)
+    * [https://github.com/hrhouma1/next-01-evaluation-du-lab2-auth-clerk-1/tree/main/06-documentation-06](https://github.com/hrhouma1/next-01-evaluation-du-lab2-auth-clerk-1/tree/main/06-documentation-06)
+* Dépôt général NextAuth : [https://github.com/hrhouma/next-auth](https://github.com/hrhouma/next-auth)
+
+## “PHOTOS”
+
+* [https://github.com/hrhouma1/next-01-evaluation-lab2/tree/main/documentation/phases/phase1](https://github.com/hrhouma1/next-01-evaluation-lab2/tree/main/documentation/phases/phase1)
+* [https://github.com/hrhouma1/next-01-evaluation-du-lab2-projet-final](https://github.com/hrhouma1/next-01-evaluation-du-lab2-projet-final)
+
+## Référentiels de cours (généraux)
+
+* [https://github.com/hrhouma1/sw-1/](https://github.com/hrhouma1/sw-1/)
+* [https://github.com/hrhouma1/sw-2](https://github.com/hrhouma1/sw-2)
+* [https://github.com/hrhouma1/sw-3](https://github.com/hrhouma1/sw-3)
+
